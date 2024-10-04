62bcbcf0bc9c562df56bae16849427492dba288a
karthikeyanrajan21 <149597338+karthikeyanrajan21@users.noreply.github.com>
2024-10-04 15:49:00 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,31 +1,37 @@
-import org.junit.jupiter.api.Test;
+package com.example;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
     @Test
-    public void testGoogleTitle() throws MalformedURLException {
-        // Use ChromeOptions for browser settings
-        ChromeOptions options = new ChromeOptions();
-
-        // Get Selenium Remote URL from environment (GitHub Actions will set this)
-        String seleniumUrl = System.getenv("SELENIUM_REMOTE_URL");
-
-        // Set up WebDriver with remote URL
-        WebDriver driver = new RemoteWebDriver(new URL(seleniumUrl), options);
-        
-        // Open Google and check the title
+    public void testGoogleTitle() {
         driver.get("https://www.google.com");
-        assertEquals("Google", driver.getTitle());
+        String title = driver.getTitle();
 
-        // Close the browser
-        driver.quit();
+        // Intentional failure to show Error 1
+        // Incorrect expected title: Google has a different actual title
+        Assert.assertEquals(title, "Incorrect Title", "Error 1: Title does not match!");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
